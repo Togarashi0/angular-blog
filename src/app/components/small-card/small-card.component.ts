921fd9236f665ab2c9613bfcd7cdbf58eb9b86be
marcos <marcos.zellner3@gmail.com>
2024-02-24 21:14:27 -0300

    PROJETO FINAL

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -10,6 +10,8 @@ export class SmallCardComponent implements OnInit {
   photoCover:string = 'aaa'
   @Input()
   cardTitle:string = 'aaaa'
+  @Input()
+  Id:string = '0'
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,9 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "photo":"aaaaa",
+        "title":"New FFXIV expansion will come in 2024!!!",
+        "description": "The next expansion for FINAL FANTASY XIV Online is titled 'Dawntrail' and launches Summer 2024",
+        "photoCover": "https://gh.cdn.sewest.net/assets/ident/news/ce6d3e65/FFXIV_PUB_FANFESTIVAL_2023_in_LAS_VEGAS_00_EN-j59jq8j81.jpg?quality=65"
+    }
+]
